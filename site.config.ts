6f19bd1244f3f5cdd246553d8bdc38df020ecb4c
kforris <58551353+kforris@users.noreply.github.com>
2022-06-08 12:27:38 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'kforris website',
+  domain: 'kforris.com',
+  author: 'Kris Wang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Showcase/Portfolio site of kforris',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'kforris_w',
+  github: 'kforris',
+  linkedin: 'kris-wang-9b9b721b6',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
